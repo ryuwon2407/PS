17729fb08b7599cd459e0dd44095f0af8fbc09ea
junoyou <ryuwon_@naver.com>
2022-03-08 00:11:15 +0900

    22.03.07

---
+++ code_plus/코딩 테스트 준비_기초 - 수학/17427.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int divs_num;
+    int sum = 0;
+
+    scanf("%d", &divs_num);
+    for(int i = 1; i <= divs_num; i++)
+    {
+        for(int j = 1; j <=  / 2; j++)
+        {
+            
+        }
+    }
+    printf("%d", sum);
+}
