b3b2d30f1132e0763066a50c963fae0037e2781f
junoyou <ryuwon_@naver.com>
2022-03-08 23:36:31 +0900

    단계별 풀어보기 - 기본 수학1

---
+++ Step_by_step/8/1712.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    long long int i;
+
+    scanf("%d %d %d", &A, &B, &C);
+    if( C - B <= 0)
+        printf("-1\n");
+    else
+        printf("%d\n", A / (C - B) + 1);
+    return 0;
+}
