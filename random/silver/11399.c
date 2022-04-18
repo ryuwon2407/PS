380ca036e32b4463a0fbd4def1414ce34d789aa1
junoyou <ryuwon_@naver.com>
2022-04-18 22:15:35 +0900

    11399. ATM

---
+++ random/silver/11399.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int withdraw_time[MAX];
+
+int main()
+{
+    int N        // 사람의 수
+    ,   sum = 0  // 필요한 시간의 합의 최솟값
+    ,   temp = 0;// 임시 저장
+    
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++)
+        scanf("%d", &withdraw_time[i]);
+
+    for(int i = 0; i < N - 1; i++)
+        for(int j = 0; j < N - 1; j++)
+            if(withdraw_time[j] > withdraw_time[j + 1])
+            {
+                temp = withdraw_time[j];
+                withdraw_time[j] = withdraw_time[j + 1];
+                withdraw_time[j + 1] = temp;   
+            }
+
+    temp = 0;
+    for(int i = 0; i < N; i++)
+        sum += temp += withdraw_time[i];
+
+    printf("%d\n", sum);
+
+    return (0);
+}
