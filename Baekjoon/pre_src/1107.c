924a2c3a407cae2ebe7ed95830016122cd282d7c
junoyou <ryuwon_@naver.com>
2022-10-28 20:24:46 +0900

    Non-Pass : 1107. 리모컨

--- Baekjoon/pre_src/1107.c
+++ Baekjoon/pre_src/1107.c
@@ -21,25 +21,55 @@
 // 80000 같은 경우 70000으로 최대값이 입력됨.. => 해결
 // 99999 같은 경우 100000으로 채널 할당을 못하는 문제.. => 코드 재작성. (알고리즘 오류)
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
-#define MAX 10
+#define BUTTON_MAX 10
 
-int button[MAX];
+int button[BUTTON_MAX];
+
+
+int continuous_button_select(int start_num, int end_num, int len, int i)
+{
+    int num;
+
+    if(start_num < end_num){
+        for(num = start_num; num <= end_num; num++)
+        {
+            if(button[num] == 0){
+                if(num == 0 && i == len)
+                    continue;
+                else
+                    break;
+            }
+        }
+    }
+    else{
+        for(num = start_num; num >= end_num; num--){
+            if(button[num] == 0){
+                if(num == 0 && i == len)
+                    continue;
+                else
+                    break;
+            }
+        }
+    }
+
+    return num;
+}
 
 int main()
 {
     int N            // 이동 할 채널
     ,   M            // 고장난 버튼 수
     ,   len     = 0  // 채널 자릿수
-    ,   count   = 0  // +1, -1 클릭 버튼 수
     ,   ch_bak  = 0  // 채널 backup
-    ,   ch_temp = 0  // 채널 temp
-    ,   temp, temp_2, temp_3;
+    ,   temp = 0, temp_2 = 0, temp_3 = 0
+    ,   ch_temp = 0, ch_temp_2 = 0, ch_temp_3 = 0;
 
     scanf("%d\n%d", &N, &M);
-    while(M--)
+    for(int i = 0; i < M; i++)
     {
         scanf("%d", &temp);
         button[temp]++;
@@ -54,33 +84,20 @@ int main()
     ch_bak = N;
     for(int i = len; i > 0; i--)
     {
-        if((temp < temp_3) && i != len){
-            for(int j = 9; j >= 0; j--){
-                if(button[j] == 0)
-                {
-                    temp = j;
-                    break;
-                }
-            }
-        }
-        else if((temp > temp_3) && i != len){
-            for(int j = 0; j <= 9; j++){
-                if(button[j] == 0)
-                {
-                    temp = j;
-                    break;
-                }
-            }
-        }
+        if((temp < temp_3) && i != len && len != 1)
+            temp = continuous_button_select(9, 0, len, i);
+        else if((temp > temp_3) && i != len && len != 1)
+            temp = continuous_button_select(0, 9, len, i);
         else
         {
             temp = temp_3 = N / (int)pow(10, i - 1);
-            for(int j = 0; j < MAX * 2; j++)
+            for(int j = 0; j < BUTTON_MAX * 2; j++)
             {
                 temp_2 = temp + (int)pow(-1, j + 1) * (int)floor((j+1) / 2);
-                if(temp_2 > -1 && temp_2 < 10){
-                    if(button[temp_2] == 0)
-                    {
+                if((temp_2 > -1 && temp_2 < 10) && button[temp_2] == 0){
+                    if(temp_2 == 0 && i == len && len != 1)
+                        continue;
+                    else{
                         temp = temp_2;
                         break;
                     }
@@ -90,14 +107,32 @@ int main()
         ch_temp += temp * (int)pow(10, i - 1);
         N %= (int)pow(10, i - 1);
     }
-    
-    if(ch_temp > ch_bak)
-        for(int i = ch_temp; i > ch_bak; i--)
-            count++;
-    else if(ch_temp < ch_bak)
-        for(int i = ch_temp; i < ch_bak; i++)
-            count++;
 
-    printf("%d\n", (len + count) <= (abs(ch_bak - 100)) ? (len + count) : (abs(ch_bak - 100)));
+    if(M == 10)
+        printf("%d\n", abs(ch_bak - 100));
+    else if(len != 1){
+        // 한자리 더 많을때 채널 최저값
+        for(int i = len + 1; i > 0; i--){
+            temp = continuous_button_select(0, 9, len + 1, i);
+            ch_temp_2 += temp * (int)pow(10, i - 1);
+        }
+        
+        // 한자리 더 적을때 채널 최대값
+        for(int i = len - 1; i > 0; i--){
+            temp = continuous_button_select(9, 0, len - 1, i);
+            ch_temp_3 += temp * (int)pow(10, i - 1);
+        }
+
+        if(abs(ch_bak - ch_temp) + len < abs(ch_bak - ch_temp_2) + len - 1 &&  abs(ch_bak - ch_temp) + len < abs(ch_bak - ch_temp_3) + len - 1)
+            temp = abs(ch_bak - ch_temp) + len;
+        else if(abs(ch_bak - ch_temp_2) + len - 1 <  abs(ch_bak - ch_temp) + len && abs(ch_bak - ch_temp_2) + len - 1 < abs(ch_bak - ch_temp_3) + len - 1)
+            temp = abs(ch_bak - ch_temp_2) + len + 1;
+        else
+            temp = abs(ch_bak - ch_temp_3) + len - 1;
+        printf("%d\n", temp <= (abs(ch_bak - 100)) ? temp : (abs(ch_bak - 100)));
+    }
+    else
+        printf("%d\n",  abs(ch_bak - ch_temp) + len <= (abs(ch_bak - 100)) ?  abs(ch_bak - ch_temp) + len : (abs(ch_bak - 100)));
+
     return 0;
 }
