332a3c921b57f322262dda29d812f0520e77f3ec
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2025-11-16 22:38:29 +0900

    [Silver V] Title: 숫자 카드, Time: 1212 ms, Memory: 104656 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] cards = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(cards);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+
+            if (Arrays.binarySearch(cards, target) >= 0) sb.append("1 ");
+            else sb.append("0 ");
+        }
+
+        System.out.println(sb);
+    }
+}
