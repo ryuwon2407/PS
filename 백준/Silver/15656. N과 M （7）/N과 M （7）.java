5cf7e36304dd62c019f50b734cc0d7045543e74c
Junho Yu <95111999+ryuwon2407@users.noreply.github.com>
2025-11-26 17:11:12 +0900

    [Silver III] Title: N과 M (7), Time: 580 ms, Memory: 123288 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] num;
+
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        num = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++)
+            num[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(num); //정렬
+        dfs(new int[M],0);
+        System.out.print(sb.toString());
+    }
+
+    static void dfs(int[] arr, int m){
+        if(m == M){
+            for(int i = 0; i < M; i++) sb.append(arr[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for(int i = 0; i < N; i++){
+            arr[m] = num[i];
+            dfs(arr, m+1);
+        }
+    }
+}
